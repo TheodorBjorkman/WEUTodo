0e1dc396c7fae44ea7345b307510f5086890dde6
TheodorBjorkman <70270703+TheodorBjorkman@users.noreply.github.com>
2021-01-19 18:50:51 +0100

    Major functionality update
    Fixade clear selected
    clear all
    tillägning av objekt vid tryck av enter
    clearing av textfält vid tillägg av objekt

--- JS/script.js
+++ JS/script.js
@@ -5,9 +5,50 @@ function addToList() {
     document.getElementById("mainList").insertAdjacentHTML("afterbegin", `<label id="${x}"><input id="button${x}" type="checkbox">${input}`);
     console.log(input);
     x++;
+    console.log("X is now: " + x) + ". Clearing input";
+    document.getElementById("input").value = "";
 }
 
 function clearSelected() {
-    var y = 0;
-    console.log(document.getElementById(`button${y}`).value);
+    let a = 0;
+    console.log("Removing selected elements");
+    for (let i = 0; i < x; i++) {
+        console.log("Checking " + i + "(" + document.getElementById(`button${i}`) + ")");
+        if (document.getElementById(`button${i}`).checked) {
+            console.log("Removing " + i + "(" + document.getElementById(`button${i}`) + ")");
+            document.getElementById(`${i}`).remove();
+            a++;
+        }
+    }
+    reasignID();
+    x -= a;
+    console.log("Amount of removed items: " + a + ". X is now: " + x);
+}
+
+function clearAll() {
+    if (confirm("Are you sure you wish to remove all items?")) {
+        for (i = 0; i < x; i++) {
+            console.log("Removing " + i + "(" + document.getElementById(`button${i}`) + ")");
+            document.getElementById(`${i}`).remove();
+        }
+        console.log("Removed all elements, resetting x to 0")
+        x = 0;
+    }
+}
+
+function reasignID() {
+    console.log("Reasigning ids");
+    for (let i = 0; i < x; i++) {
+        console.log("Assigning to " + i);
+        for (let j = i; j < x; j++) {
+            console.log("Checking " + j + "(" + document.getElementById(`button${j}`) + ")");
+            if (document.getElementById(`${j}`)) {
+                console.log(j + "(" + document.getElementById(`button${j}`) + ")" + "has been assigned to" + i);
+                document.getElementById(`${j}`).id = `${i}`;
+                document.getElementById(`button${j}`).id = `button${i}`;
+                break;
+            }
+        }
+    }
+    console.log("Reasigning complete. Reminder x = " + x);
 }
