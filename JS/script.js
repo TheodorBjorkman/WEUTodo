53a3acc0f68e5e53c6d32f24d2e865707c6f847e
TheodorBjorkman <70270703+TheodorBjorkman@users.noreply.github.com>
2021-03-06 22:25:40 +0100

    Big update
    Added:
    Local save
    Animation (with on/off box)
    Clickable text instead of checkboxes and clear selected
    Changeable mode between ul and ol
    Slight aesthetic updates

--- JS/script.js
+++ JS/script.js
@@ -1,63 +1,114 @@
 var x = 0;
 
+const LOCAL_STORAGE_KEY_TODO = "app.todo";
+
 function addToList() {
-    var input = document.getElementById("input").value;
-    if (input === "" || document.getElementById("red").checked) {} else {
-        document.getElementById("listNormal").insertAdjacentHTML("beforeend", `<label id="${x}" class="lable"><input id="button${x}" type="checkbox">${input}</label><div class="smol">`);
-        console.log(input);
+    var item = new Item(document.getElementById("input").value, document.getElementById("red").checked);
+    if (item.text === "" || item.important) {} else {
+        document.getElementById("listNormal").insertAdjacentHTML("beforeend", `<li id="${x}" class="item" onclick="clearSelected(${x})">${item.text}</li><div class="smol" id="smol${x}">`);
         x++;
-        console.log("X is now: " + x) + ". Clearing input";
         document.getElementById("input").value = "";
     }
-    if (input === "" || !document.getElementById("red").checked) {} else {
-        document.getElementById("listRed").insertAdjacentHTML("beforeend", `<label id="${x}" class="lableRed"><input id="button${x}" type="checkbox">${input}</label><div class="smol">`);
-        console.log(input);
+    if (item.text === "" || !item.important) {} else {
+        document.getElementById("listImportant").insertAdjacentHTML("beforeend", `<li id="${x}" class="itemRed" onclick="clearSelected(${x})">${item.text}</li><div class="smol" id="smol${x}">`);
         x++;
-        console.log("X is now: " + x) + ". Clearing input";
         document.getElementById("input").value = "";
     }
+    store();
 }
 
-function clearSelected() {
-    let a = 0;
-    console.log("Removing selected elements");
-    for (let i = 0; i < x; i++) {
-        console.log("Checking " + i + "(" + document.getElementById(`button${i}`) + ")");
-        if (document.getElementById(`button${i}`).checked) {
-            console.log("Removing " + i + "(" + document.getElementById(`button${i}`) + ")");
-            document.getElementById(`${i}`).remove();
-            a++;
-        }
-    }
-    reasignID();
-    x -= a;
-    console.log("Amount of removed items: " + a + ". X is now: " + x);
+async function clearSelected(i) {
+    animatedRemoval(`${i}`)
 }
 
 function clearAll() {
     if (confirm("Are you sure you wish to remove all items?")) {
         for (i = 0; i < x; i++) {
-            console.log("Removing " + i + "(" + document.getElementById(`button${i}`) + ")");
-            document.getElementById(`${i}`).remove();
+            animatedRemoval(`${i}`)
         }
-        console.log("Removed all elements, resetting x to 0")
         x = 0;
+        store();
     }
 }
 
-function reasignID() {
-    console.log("Reasigning ids");
-    for (let i = 0; i < x; i++) {
-        console.log("Assigning to " + i);
-        for (let j = i; j < x; j++) {
-            console.log("Checking " + j + "(" + document.getElementById(`button${j}`) + ")");
-            if (document.getElementById(`${j}`)) {
-                console.log(j + "(" + document.getElementById(`button${j}`) + ")" + "is being assigned to" + i);
-                document.getElementById(`${j}`).id = `${i}`;
-                document.getElementById(`button${j}`).id = `button${i}`;
-                break;
-            }
+function animatedRemoval(input) {
+    if(document.getElementById(input))
+    {
+        document.getElementById(input).style.animation = "fade 500ms";
+        if(document.getElementById('animation').checked)
+        {
+            document.getElementById(input).style.animation += "shrink 500ms 500ms";
+            document.getElementById("smol"+input).style.animation = "shrink 500ms 500ms";
+            delayedRemoval(499, input);
+            delayedRemoval(999, 'smol'+input);
+        } else 
+        {
+            delayedRemoval(0000, input);
+            delayedRemoval(0000, 'smol'+input);
         }
     }
-    console.log("Reasigning complete. Reminder x = " + x);
+}
+
+function timedRemoval(time, input) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+          document.getElementById(input).remove();
+          store();
+          resolve('resolved');
+      }, time);
+    });
+  }
+  
+async function delayedRemoval(time, input) {
+    await timedRemoval(time, input);
+}
+
+class Item {
+    constructor(text, important) {
+        this.text = text;
+        this.important = important;
+    }
+}
+
+function fetch() {
+    var list = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_TODO));
+    for (let index = 0; index < list.length; index++) {
+        var test = list[index];
+        var cha = test.charAt(test.length-5);
+        if (test.endsWith('.item')) {
+            document.getElementById("listNormal").insertAdjacentHTML("beforeend", `<li id="${x}" class="item" onclick="clearSelected(${x})">${test.slice(0, test.length - 5)}</li><div class="smol" id="smol${x}">`);
+            x++;
+            document.getElementById("input").value = "";
+        } else 
+        {
+            document.getElementById("listImportant").insertAdjacentHTML("beforeend", `<li id="${x}" class="itemRed" onclick="clearSelected(${x})">${test.slice(0, test.length - 8)}</li><div class="smol" id="smol${x}">`);
+            x++;
+            document.getElementById("input").value = "";
+        }
+    }
+}
+
+function store() {
+    var list = [];
+    for(var i = 0; i < x; i++)
+    {
+        if(document.getElementById(i)) list.push(document.getElementById(i).innerText + '.' + document.getElementById(i).className);
+    }
+    localStorage.setItem(LOCAL_STORAGE_KEY_TODO, JSON.stringify(list));
+}
+
+function mode() {
+    var e = document.getElementById("parent").innerHTML;
+    console.log(e);
+    if(e.includes('<ol')) 
+    {
+        e = e.replaceAll('<ol', '<ul');
+        e = e.replaceAll('</ol', '</ul');
+    } else
+    {
+        e = e.replaceAll('<ul', '<ol');
+        e = e.replaceAll('</ul', '</ol');
+    }
+    console.log(e);
+    document.getElementById("parent").innerHTML = e;
 }
